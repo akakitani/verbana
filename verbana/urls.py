aa5544621b9cb8eaeeabdfb448e0653992d10d7f
akakitani <atsunori.kakitani@gmail.com>
2013-12-11 23:01:29 -0800

    Fix favicon bug

--- verbana/urls.py
+++ verbana/urls.py
@@ -10,8 +10,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
+    url(r'^robots\.txt$', 'django.views.generic.simple.direct_to_template', {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/staticfiles/images/favicon.ico'), name='favicon'),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^', RedirectView.as_view(url='/'), name='redirect'),
-    url(r'^robots\.txt$', 'django.views.generic.simple.direct_to_template', {'template': 'robots.txt', 'mimetype': 'text/plain'}),
-    url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/images/favicon.ico'}),
 )
